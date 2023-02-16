0f469a5c0cc7938b4504e113a9385a8904e2bdea
apple <apple@appleui-MacBookPro.local>
2023-02-16 13:38:23 +0900

    글로벌스타일시트

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -2,10 +2,21 @@ import styled from 'styled-components';
 
 const FooterMain = styled.footer`
   width: 100%;
+  background-color: var(--backColor);
+  height: 200px;
+`;
+const FooterBody = styled.div`
+  margin: 0 auto;
+  max-width: 70%;
+  height: 200px;
 `;
 
 function Footer() {
-  return <FooterMain />;
+  return (
+    <FooterMain>
+      <FooterBody />
+    </FooterMain>
+  );
 }
 
 export default Footer;
--- src/style/GlobalStyles.js
+++ src/style/GlobalStyles.js
@@ -6,6 +6,9 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  :root {
+    --backColor : #7749F8;
+  }
 `;
 
 export default GlobalStyle;
