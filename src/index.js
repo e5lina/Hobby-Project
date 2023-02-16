9bcfa76c4c34ab2e11d8013994faf66b50b3e04e
apple <apple@appleui-MacBookPro.local>
2023-02-16 12:37:51 +0900

    글로벌 스타일시트

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import GlobalStyle from './GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <GlobalStyle />
     </BrowserRouter>
   </React.StrictMode>,
 );
--- src/page/main/Main.jsx
+++ src/page/main/Main.jsx
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
+import Footer from '../../component/footer/Footer';
+import Header from '../../component/header/Header';
 
 const MainBody = styled.main`
   width: 100%;
   height: 300px;
-  background-color: black;
+  border: 1px solid red;
 `;
 
 function Main() {
-  return <MainBody />;
+  return (
+    <MainBody>
+      <Header />
+      <Footer />
+    </MainBody>
+  );
 }
 
 export default Main;
